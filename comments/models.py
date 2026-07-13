1320934601209605be94db045cf71fe53ecbd0c2
kiyat1724 SAID HASSANI <r.saidhassani@outlook.fr>
2026-07-13 12:13:56 +0200

    Ajout du modèle Comments

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from issues.models import Issue
 # Create your models here.
+
+
+class Comment(models.Model):
+    description = models.TextField()
+    author_user = models.ForeignKey(
+        User,
+        on_delete=models.PROTECT
+    )
+    issue = models.ForeignKey(
+        Issue,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    created_time = models.DateTimeField(
+        auto_now_add=True
+    )
+  
+    def __str__(self):
+        return f"Comment by {self.author_user.username}"
