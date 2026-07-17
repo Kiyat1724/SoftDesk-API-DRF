9463a41662863ccba95f9b5776d69bedb708c6a5
kiyat1724 SAID HASSANI <r.saidhassani@outlook.fr>
2026-07-17 10:57:26 +0200

    Ajout des viewsets des applications

--- issues/views.py
+++ issues/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from rest_framework.viewsets import ModelViewSet
+from .models import Issue
+from .serializers import IssueSerializer
 # Create your views here.
+
+
+class IssueViewSet(ModelViewSet):
+    queryset = Issue.objects.all()
+    serializer_class = IssueSerializer
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
+from rest_framework.viewsets import ModelViewSet
+from .models import Project, Contributor
+from .serializers import ProjectSerializer, ContributorSerializer
 # Create your views here.
+
+
+class ProjectViewSet(ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+
+class ContributorViewSet(ModelViewSet):
+    queryset = Contributor.objects.all()
+    serializer_class = ContributorSerializer
