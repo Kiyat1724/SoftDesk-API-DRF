bc9739a326fb4779590c8c7d18196682eef66fec
kiyat1724 SAID HASSANI <r.saidhassani@outlook.fr>
2026-07-17 11:58:12 +0200

    Configuration des routes de l'API avec les routeurs DRF

---
+++ projects/urls.py
@@ -0,0 +1,9 @@
+from rest_framework.routers import DefaultRouter
+from .views import ProjectViewSet, ContributorViewSet
+
+router = DefaultRouter()
+
+router.register(r'projects', ProjectViewSet)
+router.register(r'contributors', ContributorViewSet)
+
+urlpatterns = router.urls
